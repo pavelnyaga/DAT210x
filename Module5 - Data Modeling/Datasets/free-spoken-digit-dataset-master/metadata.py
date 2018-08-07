6cc89b7f8754bfbf4ae7e045178c3382ab6262f1
Pavel Nyaga <pavel.nyaga@gmail.com>
2018-08-07 16:26:23 -0500

    upload to github

---
+++ Module5 - Data Modeling/Datasets/free-spoken-digit-dataset-master/metadata.py
@@ -0,0 +1,24 @@
+"""A python dictionary to store meta-data about the speakers."""
+
+metadata = {
+    'jackson': {
+        'gender': 'male',
+        'accent': 'USA/neutral',
+        'language': 'english'
+    },
+    'nicolas': {
+    	'gender': 'male',
+    	'accent': 'BE/French',
+    	'language': 'english'
+    },
+    'theo': {
+    	'gender': 'male',
+    	'accent': 'USA/neutral',
+    	'language': 'english'
+    },
+    'jason': {
+    	'gender': 'male',
+    	'accent': 'USA/neutral',
+    	'language': 'english'
+    }
+}
